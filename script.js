52145087ce49666ba4612e4198cc4be6518c8d5c
Daniela Farias <farias.danicris@gmail.com>
2021-10-29 00:31:59 -0300

    Alteração do iframe

--- script.js
+++ script.js
@@ -14,19 +14,34 @@ const humorsText = {
 };
 
 const humorsImage = {
-    0: "images/boa-feliz.jpg",
-    1: "images/boa-baddass.jpg",
-    2: "images/boa-brava.jpg",
-    3: "images/boa-cansada.jpg",
-    4: "images/boa-engraçada.jpg",
-    5: "images/boa-estudiosa.png",
-    6: "images/boa-faminta.jpg",
-    7: "images/boa-fofa.jpg",
-    8: "images/boa-julgando.jpg",
-    9: "images/boa-meh.jpg",
-    10: "images/boa-pensativa.jpg",
-    11: "images/boa-timida.jpg",
-  };
+  0: "images/boa-feliz.jpg",
+  1: "images/boa-baddass.jpg",
+  2: "images/boa-brava.jpg",
+  3: "images/boa-cansada.jpg",
+  4: "images/boa-engraçada.jpg",
+  5: "images/boa-estudiosa.png",
+  6: "images/boa-faminta.jpg",
+  7: "images/boa-fofa.jpg",
+  8: "images/boa-julgando.jpg",
+  9: "images/boa-meh.jpg",
+  10: "images/boa-pensativa.jpg",
+  11: "images/boa-timida.jpg",
+};
+
+const humorsMusic = {
+  0: "https://www.youtube.com/embed/qE05Bv1e_fo",
+  1: "https://www.youtube.com/embed/bKqe9zqMUcI",
+  2: "https://www.youtube.com/embed/pykyNOEVqCU",
+  3: "https://www.youtube.com/embed/hhBwPj7NpBk",
+  4: "https://www.youtube.com/embed/M48JMEkiu3Q",
+  5: "https://www.youtube.com/embed/Q3aLp8nhXfg",
+  6: "https://www.youtube.com/embed/fOxl2OIOVII",
+  7: "https://www.youtube.com/embed/skbnuIhVQUA",
+  8: "https://www.youtube.com/embed/RlMg8FbcP7g",
+  9: "https://www.youtube.com/embed/mR8i0fRVmSA",
+  10: "https://www.youtube.com/embed/PQjovLrnvVo",
+  11: "https://www.youtube.com/embed/o1XOFi42xZM",
+};
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -37,8 +52,9 @@ function getRandomInt(min, max) {
 const textHumor = document.querySelector("#btn-change");
 
 textHumor.addEventListener("click", function () {
-  var index = getRandomInt(0, 11);
+  var index = getRandomInt(0, 11 + 1);
 
   document.getElementById("actual-humor").innerHTML = humorsText[index];
   document.getElementById("boa-photo").src = humorsImage[index];
+  document.getElementById("youtube-video").src = humorsMusic[index];
 });
